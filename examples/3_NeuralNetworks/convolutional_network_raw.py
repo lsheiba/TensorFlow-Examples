d12bce47638379844a0ee9a98d0fd43e935f23eb
Leonard Sheiba <lsheiba@gmail.com>
2017-12-04 19:54:02 -0800

    Update convolutional_network_raw.py

--- examples/3_NeuralNetworks/convolutional_network_raw.py
+++ examples/3_NeuralNetworks/convolutional_network_raw.py
@@ -13,8 +13,9 @@ from __future__ import division, print_function, absolute_import
 import tensorflow as tf
 
 # Import MNIST data
+import os
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+mnist = input_data.read_data_sets(os.environ['DATA_DIR'], one_hot=True)
 
 # Training Parameters
 learning_rate = 0.001
